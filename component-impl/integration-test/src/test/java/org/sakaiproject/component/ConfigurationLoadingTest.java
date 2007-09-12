115276ec7285399863b906114a7fe0d89ad87d6f
ray@media.berkeley.edu <ray@media.berkeley.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2007-09-12 23:35:14 +0000

    Get component configuration test in place
    git-svn-id: https://source.sakaiproject.org/svn/component/branches/SAK-8315@35019 66ffb92e-73f9-0310-93c1-f5514f145a0a

---
+++ component-impl/integration-test/src/test/java/org/sakaiproject/component/ConfigurationLoadingTest.java
@@ -0,0 +1,87 @@
+/**********************************************************************************
+*
+* $Id$
+*
+***********************************************************************************
+*
+* Copyright (c) 2007 The Regents of the University of California
+*
+* Licensed under the Educational Community License, Version 1.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.opensource.org/licenses/ecl1.php
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+**********************************************************************************/
+
+package org.sakaiproject.component;
+
+import java.net.URL;
+
+import junit.extensions.TestSetup;
+import junit.framework.Assert;
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.sakaiproject.component.api.ServerConfigurationService;
+import org.sakaiproject.component.test.ITestComponent;
+import org.sakaiproject.test.SakaiTestBase;
+
+/**
+ *
+ */
+public class ConfigurationLoadingTest extends SakaiTestBase {
+	private static Log log = LogFactory.getLog(ConfigurationLoadingTest.class);
+	
+	private ServerConfigurationService serverConfigurationService;
+	
+	public static Test suite() {
+		TestSetup setup = new TestSetup(new TestSuite(ConfigurationLoadingTest.class)) {
+			protected void setUp() throws Exception {
+				initializeSakaiHome();
+				try {
+					oneTimeSetup();
+				} catch (Exception e) {
+					log.warn(e);
+				}
+			}
+			protected void tearDown() throws Exception {	
+				oneTimeTearDown();
+			}
+		};
+		return setup;
+	}
+	
+	public void setUp() throws Exception {
+		serverConfigurationService = (ServerConfigurationService)getService(ServerConfigurationService.class.getName());
+	}
+	
+	public void testSakaiProperties() throws Exception {
+		// This test assumes that your "sakai.properties" file sets
+		// "loadedTomcatSakaiProperties" to true, doesn't change the
+		// default "gatewaySiteId" property, and changes the default value of a
+		// TestComponent string property.
+		Assert.assertTrue(serverConfigurationService.getString("loadedTomcatSakaiProperties").equals("true"));
+		Assert.assertTrue(serverConfigurationService.getString("gatewaySiteId").equals("!gateway"));
+		ITestComponent testComponent = (ITestComponent)getService(ITestComponent.class.getName());
+		Assert.assertTrue(testComponent.getStringProperty1().equals("nondefault"));
+	}
+	
+	public static void initializeSakaiHome() {
+		URL propertiesUrl = ConfigurationLoadingTest.class.getClassLoader().getResource("sakai.properties");
+		if (log.isDebugEnabled()) log.debug("propertiesUrl=" + propertiesUrl);
+		if (propertiesUrl != null) {
+			String propertiesFileName = propertiesUrl.getFile();
+			String sakaiHomeDir = propertiesFileName.substring(0, propertiesFileName.lastIndexOf("sakai.properties"));
+			System.setProperty("test.sakai.home", sakaiHomeDir);
+		}
+	}
+}
